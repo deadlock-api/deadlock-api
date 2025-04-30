11a26b7ccd11f7c9e06204b6c82c563af29888e5
Manuel Raimann <raimannma@outlook.de>
2025-04-30 10:22:32 +0200

    Add hero matchup stats table and improve hero stats page
    Introduced `HeroesMatchupStatsTable` for displaying synergy and counter data for all heroes and enhanced the `HeroMatchupStatsTable` with filtering based on `stat` type. Added `HeroSelector` for hero selection, updated the `heroes` route for better

---
+++ app/components/hero_selector.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import type { AssetsHero } from "~/types/assets_hero";
+
+export default function HeroSelector({
+  onHeroSelected,
+  selectedHero,
+}: { onHeroSelected: (selectedHeroId: number) => void; selectedHero?: number | null }) {
+  const { data } = useQuery<AssetsHero[]>({
+    queryKey: ["assets-heroes"],
+    queryFn: () => fetch("https://assets.deadlock-api.com/v2/heroes").then((res) => res.json()),
+    staleTime: Number.POSITIVE_INFINITY,
+  });
+
+  const heroesSorted = useMemo(() => data?.sort((a, b) => a.name.localeCompare(b.name)) ?? [], [data]);
+
+  return (
+    <div className="max-w-60">
+      <label htmlFor="hero-select" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Select Hero
+      </label>
+      <select
+        id="hero-select"
+        onChange={(e) => onHeroSelected(Number(e.target.value))}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        value={selectedHero || ""}
+      >
+        {heroesSorted?.map((hero) => (
+          <option key={hero.id} value={hero.id}>
+            {hero.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
--- app/routes/heroes.tsx
+++ app/routes/heroes.tsx
@@ -1,7 +1,11 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useState } from "react";
-import HeroMatchupStatsTable from "~/components/hero_matchup_stats_table";
+import { useLocation } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import HeroMatchupStatsTable, { HeroMatchupStatsTableStat } from "~/components/hero_matchup_stats_table";
+import HeroName from "~/components/hero_name";
+import HeroSelector from "~/components/hero_selector";
 import HeroStatsTable from "~/components/hero_stats_table";
+import HeroesMatchupStatsTable from "~/components/heroes_matchup_stats_table";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,9 +14,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function Heroes() {
-  const urlTab = typeof window !== "undefined" ? new URLSearchParams(window.location.search).get("tab") : null;
-  const [tab, setTab] = useState(urlTab || "general");
+export default function Heroes({ initialTab }: { initialTab?: string } = { initialTab: "general" }) {
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useState<URLSearchParams | null>(new URLSearchParams(location.search));
+  useEffect(() => {
+    console.log("location", location.search);
+    const searchParams = new URLSearchParams(location.search);
+    setSearchParams(searchParams);
+
+    const searchTab = searchParams?.get("tab");
+    if (searchTab) {
+      setTab(searchTab);
+    }
+
+    const searchHeroIdString = searchParams?.get("heroId");
+    const searchHeroId = searchHeroIdString ? Number.parseInt(searchHeroIdString) : null;
+    setHeroId(searchHeroId);
+  }, [location.search]);
+
+  const searchTab = searchParams?.get("tab");
+  const [tab, setTab] = useState(searchTab || initialTab || "general");
+
+  const searchHeroIdString = searchParams?.get("heroId");
+  const searchHeroId = searchHeroIdString ? Number.parseInt(searchHeroIdString) : null;
+  const [heroId, setHeroId] = useState(searchHeroId || null);
 
   const handleTabChange = (newTab: string) => {
     setTab(newTab);
@@ -28,7 +53,7 @@ export default function Heroes() {
       <h2 className="text-3xl font-bold text-center mb-2">Hero Stats</h2>
       <p className="mb-4 text-gray-300 text-center text-sm italic">(Last 30 days)</p>
 
-      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 mb-4">
         <ul className="flex flex-wrap -mb-px">
           <li className="me-2">
             <button
@@ -47,15 +72,29 @@ export default function Heroes() {
           <li className="me-2">
             <button
               type="button"
-              onClick={() => handleTabChange("synergies")}
-              aria-current={tab === "synergies" ? "page" : undefined}
+              onClick={() => handleTabChange("matchups")}
+              aria-current={tab === "matchups" ? "page" : undefined}
               className={
-                tab === "synergies"
+                tab === "matchups"
                   ? "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
                   : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
               }
             >
-              Hero Synergies
+              Hero Matchups
+            </button>
+          </li>
+          <li className="me-2">
+            <button
+              type="button"
+              onClick={() => handleTabChange("hero-details")}
+              aria-current={tab === "hero-details" ? "page" : undefined}
+              className={
+                tab === "hero-details"
+                  ? "inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+                  : "inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+              }
+            >
+              Hero Details
             </button>
           </li>
         </ul>
@@ -66,11 +105,37 @@ export default function Heroes() {
           <HeroStatsTable columns={["winRate", "pickRate", "KDA"]} sortBy="winrate" />
         </div>
       )}
-      {tab === "synergies" && (
+      {tab === "matchups" && (
         <div className="flex flex-col gap-4">
-          <HeroMatchupStatsTable />
+          <HeroesMatchupStatsTable />
         </div>
       )}
+      {tab === "hero-details" && heroId && (
+        <>
+          <h2 className="text-2xl font-bold text-center mb-2">
+            Details for <HeroName heroId={heroId} />
+          </h2>
+          <div className="flex flex-col gap-4">
+            <HeroSelector
+              selectedHero={heroId ?? null}
+              onHeroSelected={(selectedHeroId) => {
+                setHeroId(selectedHeroId);
+                if (typeof window !== "undefined") {
+                  const url = new URL(window.location.href);
+                  url.searchParams.set("heroId", selectedHeroId.toString());
+                  window.history.pushState({}, "", url);
+                }
+              }}
+            />
+            <div className="grid grid-cols-2 gap-4">
+              <HeroMatchupStatsTable heroId={heroId} stat={HeroMatchupStatsTableStat.SYNERGY} />
+              <HeroMatchupStatsTable heroId={heroId} stat={HeroMatchupStatsTableStat.COUNTER} />
+            </div>
+          </div>
+        </>
+      )}
+      {tab}
+      {heroId}
     </>
   );
 }
