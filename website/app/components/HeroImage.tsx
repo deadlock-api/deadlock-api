a1b51d6b24d0c60be8a2a720a530ef2ccbf56789
Manuel Raimann <raimannma@outlook.de>
2025-05-13 11:54:07 +0200

    Add `className` prop to `HeroName` and `HeroImage` components for enhanced styling flexibility

--- website/app/components/HeroImage.tsx
+++ website/app/components/HeroImage.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { useMemo } from "react";
+import { cn } from "~/lib/utils";
 import type { AssetsHero } from "~/types/assets_hero";
 
-export default function HeroImage({ heroId }: { heroId: number }) {
+export default function HeroImage({ heroId, className }: { heroId: number; className?: string }) {
   const { data } = useQuery<AssetsHero[]>({
     queryKey: ["assets-heroes"],
     queryFn: () => fetch("https://assets.deadlock-api.com/v2/heroes?only_active=true").then((res) => res.json()),
@@ -12,6 +13,12 @@ export default function HeroImage({ heroId }: { heroId: number }) {
   const hero = useMemo(() => data?.find((hero) => hero.id === heroId), [data, heroId]);
 
   return (
-    <img loading="lazy" src={hero?.images?.minimap_image_webp} alt={hero?.name} title={hero?.name} className="size-6" />
+    <img
+      loading="lazy"
+      src={hero?.images?.minimap_image_webp}
+      alt={hero?.name}
+      title={hero?.name}
+      className={cn("size-6", className || "")}
+    />
   );
 }
