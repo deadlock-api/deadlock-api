9cb87fc61d8ed6e30be0cbd858f677830241fdd5
johnpyp <johnpyp.dev@gmail.com>
2025-05-13 11:33:00 -0700

    use picture for fallback to png

--- website/app/components/HeroImage.tsx
+++ website/app/components/HeroImage.tsx
@@ -18,7 +18,7 @@ export default function HeroImage({ heroId, className }: { heroId: number; class
       src={hero?.images?.minimap_image_webp}
       alt={hero?.name}
       title={hero?.name}
-      className={cn("size-6", className || "")}
+      className={cn("size-6", className)}
     />
   );
 }
--- website/app/components/ItemImage.tsx
+++ website/app/components/ItemImage.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { useMemo } from "react";
 import type { AssetsItem } from "~/types/assets_item";
+import { cn } from "../lib/utils";
 
-export default function ItemImage({ itemId }: { itemId: number }) {
+export default function ItemImage({ itemId, className }: { itemId: number; className?: string }) {
   const { data } = useQuery<AssetsItem[]>({
     queryKey: ["assets-items-upgrades"],
     queryFn: () => fetch("https://assets.deadlock-api.com/v2/items/by-type/upgrade").then((res) => res.json()),
@@ -12,6 +13,16 @@ export default function ItemImage({ itemId }: { itemId: number }) {
   const item = useMemo(() => data?.find((item) => item.id === itemId), [data, itemId]);
 
   return (
-    <img loading="lazy" src={item?.shop_image_small_webp} alt={item?.name} title={item?.name} width={36} height={36} />
+    <picture>
+      <source srcSet={item?.shop_image_small_webp} type="image/webp" />
+      <source srcSet={item?.shop_image_small} type="image/png" />
+      <img
+        loading="lazy"
+        src={item?.shop_image_small} // Fallback for browsers that don't support <picture> or neither format
+        alt={item?.name}
+        title={item?.name}
+        className={cn("size-6", className)}
+      />
+    </picture>
   );
 }
