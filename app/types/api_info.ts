92f79d25596c64a549dad53706443dcfa8c9be12
Manuel Raimann <raimannma@outlook.de>
2025-05-13 18:46:49 +0200

    Add `missed_matches` to `APIInfo` and update `_index` to display missed matches alongside fetched matches

--- app/types/api_info.ts
+++ app/types/api_info.ts
@@ -4,5 +4,6 @@ interface TableSize {
 
 export interface APIInfo {
   fetched_matches_per_day: number;
-  table_sizes: { [table: string]: TableSize };
+  missed_matches: number;
+  table_sizes?: { [table: string]: TableSize };
 }
