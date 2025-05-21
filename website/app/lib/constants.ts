7ddf6d39cd79c7d587bcb7b5596e1c0b3c75ef1d
johnpyp <johnpyp.dev@gmail.com>
2025-05-21 01:38:39 -0700

    update patch days

--- website/app/lib/constants.ts
+++ website/app/lib/constants.ts
@@ -2,20 +2,20 @@ import dayjs from "dayjs";
 
 export const PATCHES = [
   {
+    id: "2025-05-19",
+    name: "Latest Patch (2025-05-19)",
+    startDate: dayjs("2025-05-20T00:12:38Z"),
+    endDate: dayjs().endOf("day"),
+  },
+  {
     id: "2025-05-08",
-    name: "Major Patch (2025-05-08)",
+    name: "Major Item Rework (2025-05-08)",
     startDate: dayjs("2025-05-08T19:43:20Z"),
     endDate: dayjs().endOf("day"),
   },
   {
-    id: "2025-05-11",
-    name: "Latest Patch (2025-05-11)",
-    startDate: dayjs("2025-05-11T19:06:03Z"),
-    endDate: dayjs().endOf("day"),
-  },
-  {
     id: "2025-02-25",
-    name: "Previous Major Patch (2025-02-25)",
+    name: "Major Map Rework (2025-02-25)",
     startDate: dayjs("2025-02-25T21:51:13Z"),
     endDate: dayjs("2025-05-08T19:43:20Z"),
   },
