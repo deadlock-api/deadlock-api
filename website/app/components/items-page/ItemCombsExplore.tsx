e0b50e1842db6673a1ef57cf9074e091eb4ab986
Manuel Raimann <raimannma@outlook.de>
2025-06-29 16:53:43 +0200

    fix: filter display data to exclude included and excluded items

--- website/app/components/items-page/ItemCombsExplore.tsx
+++ website/app/components/items-page/ItemCombsExplore.tsx
@@ -90,7 +90,13 @@ export default function ItemCombsExplore({
   );
 
   const limitedData = useMemo(() => (limit ? sortedData?.slice(0, limit) : sortedData), [sortedData, limit]);
-  const displayData = useMemo(() => getDisplayItemStats(limitedData, assetsItems || []), [limitedData, assetsItems]);
+  const displayData = useMemo(
+    () =>
+      getDisplayItemStats(limitedData, assetsItems || []).filter(
+        (d) => !includeItems.has(d.item_id) && !excludeItems.has(d.item_id), // Filter out included and excluded items
+      ),
+    [limitedData, assetsItems, includeItems, excludeItems],
+  );
 
   if (isLoadingItemAssets) {
     return (
