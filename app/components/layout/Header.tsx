bb066a63fa029514ef33cd753c73e1c9b060f89b
Manuel Raimann <raimannma@outlook.de>
2025-11-09 01:15:33 +0100

    feat: enhance header component with tooltips for social links

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -9,17 +9,17 @@ import { DISCORD_LINK, GITHUB_LINK, PATREON_LINK, STATUS_PAGE_LINK } from "~/lib
 export function Header() {
   return (
     <header className="flex items-center justify-end p-6 bg-sidebar text-header-foreground">
-      <div className="flex items-center space-x-4">
-        <a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer">
+      <div className="flex items-center space-x-5">
+        <a href={GITHUB_LINK} target="_blank" rel="noopener noreferrer" title="Visit our GitHub">
           <GithubIcon width={24} height={24} />
         </a>
-        <a href={DISCORD_LINK} target="_blank" rel="noopener noreferrer">
+        <a href={DISCORD_LINK} target="_blank" rel="noopener noreferrer" title="Join our Discord">
           <DiscordIcon width={24} height={24} />
         </a>
-        <a href={STATUS_PAGE_LINK} target="_blank" rel="noopener noreferrer">
+        <a href={STATUS_PAGE_LINK} target="_blank" rel="noopener noreferrer" title="Status">
           <StatusIcon width={24} height={24} />
         </a>
-        <a href={PATREON_LINK} target="_blank" rel="noopener noreferrer">
+        <a href={PATREON_LINK} target="_blank" rel="noopener noreferrer" title="Support us on Patreon">
           <PatreonIcon width={24} height={24} />
         </a>
       </div>
