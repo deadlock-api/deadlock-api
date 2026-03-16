4eaa2c37a41385f56070d6b6314f3d7c69b9c2eb
Manuel Raimann <raimannma@outlook.com>
2026-03-16 18:41:41 +0100

    feat: add new big patch date for March 2026

--- api/src/routes/v1/patches/big_patch_days.rs
+++ api/src/routes/v1/patches/big_patch_days.rs
@@ -4,6 +4,7 @@ use axum::response::IntoResponse;
 use crate::error::APIResult;
 
 const BIG_PATCH_DAYS: &[&str] = &[
+    "2026-03-11T04:39:40Z",
     "2026-01-21T02:10:58Z",
     "2025-08-18T20:43:52Z",
     "2025-07-29T22:22:52Z",
