c8ef7264c4c717d01c05bcf09b597acbfde6238b
Manuel <raimannma@outlook.de>
2026-07-31 00:16:42 +0200

    Increase cache time

--- api/src/routes/v1/players/mod.rs
+++ api/src/routes/v1/players/mod.rs
@@ -193,7 +193,7 @@ pub(super) fn router() -> OpenApiRouter<AppState> {
         .merge(mmr::router())
         .merge(steam::router())
         .layer(
-            CacheControlMiddleware::new(Duration::from_mins(5))
-                .with_stale_while_revalidate(Duration::from_mins(5)),
+            CacheControlMiddleware::new(Duration::from_mins(10))
+                .with_stale_while_revalidate(Duration::from_mins(10)),
         )
 }
